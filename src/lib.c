58c5b5f950d9fbaef831f18c42b6badd97f8c890
wangjiaming <604227650@qq.com>
2017-06-16 16:34:18 +0800

    add readn

--- src/lib.c
+++ src/lib.c
@@ -20,6 +20,24 @@ size_t readline(int filedes, void *buff, size_t maxlen);
 ssize_t readn(int fd, void *vptr, size_t n){
     size_t      nleft;
     ssize_t     nread;
-
+	char	 	*ptr;
+	
+	ptr = vptr;
+	nleft = n;
+	while(nleft > 0){
+		if((nread = read(fd, ptr, nleft)) < 0){
+			if(errno == EINTR)
+				nread = 0;
+			else
+				return -1;
+		}else if(nread == 0)
+			break;
+		nletf -= nread;
+		ptr += nread;
+	}
+	return n - nleft;
 }
 
+ssize_t writen(int filedes, const void *buff, size_t nbytes){
+	
+}
