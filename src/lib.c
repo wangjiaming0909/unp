9c347f3538f5322a9e5f1ec6c51dd7a3586c8cd5
wangjiaming <604227650@qq.com>
2017-06-17 09:50:17 +0800

    writen

--- src/lib.c
+++ src/lib.c
@@ -16,18 +16,24 @@ size_t readline(int filedes, void *buff, size_t maxlen);
 * typedef __ssize_t ssize_t;
 * #define __size_t_defined
 * #endif
+
+	__STD_TYPE __SSIZE_T_TYPE __ssize_t; --->typedef int __ssize_t
+	# define __STD_TYPE		typedef
+	#define __SSIZE_T_TYPE		__SWORD_TYPE
+	# define __SWORD_TYPE		int
 * * */
-ssize_t readn(int fd, void *vptr, size_t n){
-    size_t      nleft;
+ssize_t readn(int fd, void *vptr, size_t nbytes){
+    size_t      nleft;//size_t long unsigned int
     ssize_t     nread;	
 	char	 	*ptr;
 	
 	ptr = vptr;
-	nleft = n;
+	nleft = nbytes;
 	while(nleft > 0){
 		if((nread = read(fd, ptr, nleft)) < 0){
 			if(errno == EINTR)/*EINTR, the call was interrupted by \
 			a signal before any data was read*/
+				err_msg("nread error: %s\n", strerror(errno));
 				nread = 0;
 			else
 				return -1;
@@ -40,6 +46,27 @@ ssize_t readn(int fd, void *vptr, size_t n){
 }
 
 ssize_t writen(int filedes, const void *buff, size_t nbytes){
-	size_t
+	size_t 	nleft;
+	ssize_t nwrite;
+	char 	*ptr;
 	
+	ptr = buff;
+	nleft = nbytes;
+
+	while(nleft > 0){
+		if((nwrite = write(filedes, ptr, nleft)) < 0){
+			if(nwrite < 0 && errno == EINTR)
+				err_msg("nwrite error: %s\n", strerror(errno));
+				nwrite = 0;
+			else
+				return -1;
+		}
+		nleft -= nwrite;
+		ptr += nwrite;
+	}
+	return n;
+}
+
+ssize_t readline(int fd, void *vptr, size_t maxlen){
+
 }
