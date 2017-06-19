63aa749c41b66b3db513e3d4c70e199acd89f58e
wangjiaming0909 <604227650@qq.com>
2017-06-19 20:16:35 +0800

    echo server and client

---
+++ src/tmp/test.c
@@ -0,0 +1,11 @@
+#include "unp.h"
+
+int main(){
+    
+    char buf[MAXLINE];
+    while(1){
+        if(fgets(buf, MAXLINE, stdout) != NULL)
+            printf("%s", buf);
+    }
+    return 0;
+}
