74b517814708829e6cea5480cb59610fc97909c0
wangjiaming0909 <604227650@qq.com>
2017-06-20 15:27:00 +0800

    waitpid

--- src/signal.c
+++ src/signal.c
@@ -21,11 +21,28 @@ Sigfunc *signal(int signo, Sigfunc *func){
 }
 
 //SIG_CHLD信号的处理函数
-void sig_chld(int signo){
+/*问题：当高并发时，可能存在多个套接字同时发送FIN，服务器收到第一个FIN时，其对应的子进程终止,
+    父进程接收到SIGCHLD信号，调用sig_chld函数， 同时其他几个子进程也收到FIN，
+    同时发出SIGCHLD信号，此时，屏蔽了SIGCHLD信号，因此其他几个SIGCHLD信号处于未决状态，由于
+    UNIX信号一般是不排队的，因此，最后可能执行完第一个sig_chld之后其他的信号合并为一个，
+    也就是存在有的信号终止的子进程不能被wait，成为僵尸进程。
+*/
+void sig_chld01(int signo){
     pid_t   pid;//子进程pid
     int     stat;//子进程退出状态
 
     pid = wait(&stat);
     err_msg("child %d terminated", pid);
+    return ;
+}
+
+//SIG_CHLD信号处理函数
+/*改进： 不使用wait， 使用waitpid函数*/
+void sig_chld(int signo){
+    pid_t   pid;
+    int     stat;
+    while((pid = waitpid(-1, &stat, WNOHANG)) > 0){/*小于0表示出错， 等于0表示没有等到子进程*/
+        printf("child %d teminated\n", pid);
+    }
     return ;
 }
--- src/tcpcli1.c
+++ src/tcpcli1.c
@@ -10,10 +10,8 @@ void str_cli(FILE *fp, int sockfd){
         fputs(recvline, stdout);
     }
 }
-
-int main(int argc, char **argv){
-    int sockfd;
-    struct sockaddr_in servaddr;
+int maintcpcli01(int argc, char **argv){
+    int sockfd;    struct sockaddr_in servaddr;
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -24,5 +22,22 @@ int main(int argc, char **argv){
     connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
     err_msg("connect success...");
     str_cli(stdin, sockfd);
+    exit(0);
+}
+
+int main(int argc, char **argv){
+    int i, sockfd[5];
+    struct sockaddr_in servaddr;
+
+    for(i = 0; i < 5; i++){
+        sockfd[i] = socket(AF_INET, SOCK_STREAM, 0);
+        bzero(&servaddr, sizeof(servaddr));
+        servaddr.sin_family = AF_INET;
+        servaddr.sin_port = htons(1001);
+        inet_pton(AF_INET, "139.224.239.147", &servaddr.sin_addr);
+
+        connect(sockfd[i], (struct sockaddr *)&servaddr, sizeof(servaddr));
+    }
+    str_cli(stdin, sockfd[0]);
     exit(0);
 }
