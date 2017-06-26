75cc0c8831c06ba82e569cc534a0f043f1991b95
wangjiaming0909 <604227650@qq.com>
2017-06-26 14:14:54 +0800

    cpp

--- cpp/1.cc
+++ cpp/1.cc
@@ -1,7 +1,23 @@
 #include <iostream>
 using namespace std;
 
+int sum_arr(int arr[], int n){
+	cout << "123" <<endl;
+	cout <<"sizeof arr[]:" <<  sizeof(arr) << endl << n << endl;//8
+//	cout << arr[0] << endl << arr[1] << endl << arr [2] << endl;
+//	cout << arr << endl << arr + 1 << endl << arr + 2 << endl;
+	return 0;
+}
+
 int main(){
+	int  buf[32] = {2};
+	*(buf + 1) = 12;
+	*(buf + 2)= 13;
+	cout  << &buf << endl;//0x7ffd77d21630
+	//sum_arr(buf, 1);
+	auto p2 = sum_arr;
+	p2(buf, 1);
+	cout << "sizeof buf[1]:" << sizeof(buf) << endl;
     cout << "hello world" << endl;
     return 0;
 }
