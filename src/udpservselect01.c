bd84e290084c3f84028829d68d696f6122dcf937
wangjiaming0909 <604227650@qq.com>
2017-07-04 10:09:45 +0800

    asdadsasd

--- src/udpservselect01.c
+++ src/udpservselect01.c
@@ -1,11 +1,12 @@
 #include "unp.h"
+#include "headers.h"
 
-int main(int argc, char **argv){
+int mainudp(int argc, char **argv){
     int     listenfd, connfd, udpfd, nready, maxfdp1;
     char    mesg[MAXLINE];
     pid_t   childpid;
     fd_set  rset;
-    ssize_t len;
+    ssize_t n;
     socklen_t len;
     const int on = 1;
     struct sockaddr_in cliaddr, servaddr;
@@ -14,11 +15,35 @@ int main(int argc, char **argv){
     if((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
         err_quit("socket error");
     bzero(&servaddr, sizeof(servaddr));
-    servaddr.sin_fmily = AF_INET;
+    servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(1001);
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
     setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
 
-    bind(listenfd, LISTENQ);
+    listen(listenfd, LISTENQ);
+    udpfd = socket(AF_INET, SOCK_DGRAM, 0);
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(1001);
+
+    bind(udpfd, (SA*)&servaddr, sizeof(servaddr));
+
+    signal(SIGCHLD, sig_chld);
+    FD_ZERO(&rset);
+    maxfdp1 = max(listenfd, udpfd) + 1;
+    for(;;){
+    	FD_SET(udpfd, &rset);
+    	FD_SET(udpfd, &rset);
+    	if((nready = select(maxfdp1, &rset, NULL, NULL, NULL)) < 0){
+    		if(errno == EINTR)
+    			continue;
+    		else
+    			err_quit("select error");
+    	}
+    	if(FD_ISSET(listenfd, &rset)){
+    		len = sizeof(cliaddr);
+    		connfd = accept(listenfd, (SA*)&cliaddr, &len);
+    	}
+    }
 }
