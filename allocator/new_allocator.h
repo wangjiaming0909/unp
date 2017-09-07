85625f3bcc1278a211028b93a2a4a5b92d54d1ad
wangjiaming0909 <604227650@qq.com>
2017-09-07 19:35:45 +0800

    new_allocator.h

---
+++ allocator/new_allocator.h
@@ -0,0 +1,10 @@
+/*************************************************************************
+	> File Name: new_allocator.h
+	> Author: 
+	> Mail: 
+	> Created Time: Thu 07 Sep 2017 07:35:31 PM CST
+ ************************************************************************/
+
+#ifndef _NEW_ALLOCATOR_H
+#define _NEW_ALLOCATOR_H
+#endif
