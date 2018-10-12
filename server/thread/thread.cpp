df7a2073be460bdf0d95925657748363de041a2d
www <www@debian.debian>
2018-10-12 21:49:25 +0800

    add some info of thread to thread.cpp

--- server/thread/thread.cpp
+++ server/thread/thread.cpp
@@ -1,7 +1,9 @@
 #include "thread.h"
 
+template <typename THREAD_IMP, typename THREAD_ID_TYPE>
 struct ThreadInfo{
-    const util::string thread_name;
+    const util::string      thread_name;
+    THREAD_ID_TYPE          thread_id;          
     ThreadInfo(const util::string& name) : thread_name(name){}
 };
 
