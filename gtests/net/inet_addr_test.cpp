f9e10706aab97dbbb0fb3988a769318e2195f345
wangjiaming0909 <604227650@qq.com>
2018-11-09 19:26:43 +0800

    test for string_to_addr

--- gtests/net/inet_addr_test.cpp
+++ gtests/net/inet_addr_test.cpp
@@ -20,12 +20,28 @@ TEST(test_string_to_addr, give_the_right_ip_and_port){
     if(ret != 0) FAIL();
 }
 
-TEST(test_string_to_addr, give_wrong_ip_or_port){
-    boost::scoped_array<char> wrong_ip_and_port{new char[16]};
-    snprintf(wrong_ip_and_port.get(), 16, "%s", "127.2.2.1234:22");
+TEST(test_string_to_addr, give_wrong_ip){
+    boost::scoped_array<char> wrong_ip{new char[16]};
+    snprintf(wrong_ip.get(), 16, "%s", "127.2.2.1234:22");
     sockaddr_in ret_addr;
-    int ret = net::string_to_addr(wrong_ip_and_port.get(), &ret_addr, AF_INET);
+    int ret = net::string_to_addr(wrong_ip.get(), &ret_addr, AF_INET);
     ASSERT_EQ(ret_addr.sin_family, AF_INET);
-    ASSERT_EQ(ret_addr.sin_port, htons(22));
-    //assert that addr is not right
+    //when the addr format is not right, it will not parse the port
+    // ASSERT_EQ(ret_addr.sin_port, htons(22));
+    ASSERT_EQ(ret, 0);
+}
+
+TEST(test_string_to_addr, give_zero_port_or_ADDR_ANY){
+    boost::scoped_array<char> zero_port{new char[16]};
+    snprintf(zero_port.get(), 16, "%s", "127.0.0.1:0");
+    sockaddr_in ret_addr;
+    int ret = net::string_to_addr(zero_port.get(), &ret_addr, AF_INET);
+    ASSERT_EQ(ret_addr.sin_family, AF_INET);
+    // ASSERT_EQ(ret, -1);
+    boost::scoped_array<char> ip_str{new char[16]};
+    memset(ip_str.get(), 0, 16);
+    const char* ret_addr_ptr = inet_ntop(AF_INET, &(ret_addr.sin_addr), ip_str.get(), sizeof(sockaddr_in));
+    ret = memcmp("127.0.0.1", ret_addr_ptr, strlen(ret_addr_ptr));
+    ASSERT_EQ(ret, 0);
+    ASSERT_EQ(ret_addr.sin_port, INADDR_ANY);
 }
