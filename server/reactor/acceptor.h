8d7d50e56285ad7966dcc4721b517996ce332ad9
wangjiaming0909 <604227650@qq.com>
2018-11-29 20:03:51 +0800

    move the handle_* to public methods

--- server/reactor/acceptor.h
+++ server/reactor/acceptor.h
@@ -17,15 +17,15 @@ class reactor_acceptor : public event_handler{
 public:
     reactor_acceptor(const net::inet_addr& local_addr, IO_TYPE io_type);  
     ~reactor_acceptor();
-private:
-    int get_handle() const ;
-    int open();
-    int close();
     virtual int handle_input(int handle) override;
     virtual int handle_output(int handle) override;
     virtual int handle_timeout(int handle) override;
     virtual int handle_close(int handle) override;
     virtual int handle_signal(int handle) override;
+private:
+    int get_handle() const ;
+    int open();
+    int close();
 private:
     IO_TYPE                 io_type_;
     net::sock_acceptor	    acceptor_;
