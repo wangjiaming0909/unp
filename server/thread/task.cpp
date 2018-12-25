e3600ac6df9282e0887e213169e73689d044ca94
Jiaming Wang <jwang284@slb.com>
2018-12-25 18:04:59 +0800

    task

--- server/thread/task.cpp
+++ server/thread/task.cpp
@@ -1,1 +1,24 @@
 #include "task.h"
+
+using namespace thread;
+
+template <typename T>
+task<T>::task(thread_pool* t_pool, mq_type* msg_q) 
+    : t_pool_p_(t_pool)
+    , msg_queue_p_(msg_q){ }
+
+//pass routine_run to the threads
+template <typename T>
+int task<T>::activate(){
+
+}
+
+template <typename T>
+int task<T>::wait(){
+    // t_pool_p_->
+}
+
+template <typename T>
+int task<T>::routine_run(task* self){
+    return self->routine();
+}
--- server/thread/task.h
+++ server/thread/task.h
@@ -9,14 +9,17 @@ namespace thread{
 template <typename T>
 class task{
 public:
+    using micro_seconds = std::chrono::microseconds; 
     using mq_type = message_queue<T>;
-    task(thread_pool* t_pool, mq_type* msg_q) 
-        : t_pool_p_(t_pool)
-        , msg_queue_p_(msg_q){ }
+    task(thread_pool* t_pool, mq_type* msg_q);
     thread_pool* th_pool() {return t_pool_p_; }
     message_queue* mq() { return msg_queue_p_; }
 
-//    virtual put()
+    virtual int activate();
+    virtual int wait();
+
+   virtual int routine() = 0;
+   static int routine_run(task* self);
 private:
     //why delete ?
 //    task(const task&) = delete;
