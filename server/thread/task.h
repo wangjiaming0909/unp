26cf299f94a68d6f7679980a1f5806d24b2dd146
wangjiaming0909 <604227650@qq.com>
2018-12-25 17:29:50 +0800

    asd

--- server/thread/task.h
+++ server/thread/task.h
@@ -5,14 +5,25 @@
 
 namespace thread{
 
+//type T is the type of data
+template <typename T>
 class task{
 public:
-    task(thread_pool* t_pool, message_queue* msg_q) 
+    using mq_type = message_queue<T>;
+    task(thread_pool* t_pool, mq_type* msg_q) 
         : t_pool_p_(t_pool)
         , msg_queue_p_(msg_q){ }
+    thread_pool* th_pool() {return t_pool_p_; }
+    message_queue* mq() { return msg_queue_p_; }
+
+//    virtual put()
+private:
+    //why delete ?
+//    task(const task&) = delete;
+//    task& operator=(const task&) = delete;
 private:
     thread_pool*                t_pool_p_;
-    message_queue*              msg_queue_p_;
+    mq_type*                    msg_queue_p_;
 };
 
 
