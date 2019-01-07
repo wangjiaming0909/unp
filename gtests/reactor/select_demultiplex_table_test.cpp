79bdf5d903f3f42e40ae057e438335eafde5d037
wangjiaming0909 <604227650@qq.com>
2019-01-07 20:37:33 +0800

    test for select_demultiplex_table

--- gtests/reactor/select_demultiplex_table_test.cpp
+++ gtests/reactor/select_demultiplex_table_test.cpp
@@ -19,25 +19,67 @@ public:
 
 class select_demultiplex_table_test : public ::testing::Test{
 protected:
-    select_demultiplex_table_test()
-    : table_() , reactor_()
-    , handler1_(reactor_)
-    , handler2_(reactor_){ }
-    ~select_demultiplex_table_test() override{}
+    select_demultiplex_table_test() : table_(){ 
+        reactor_ = new MockReactor();
+        handler1_ = new MockEventHandler(*reactor_);
+        handler2_ = new MockEventHandler(*reactor_);
+    }
+    ~select_demultiplex_table_test() override{
+        delete reactor_;
+        delete handler1_;
+        delete handler2_;
+    }
     void SetUp() override{ }
     void TearDown() override{}
 protected:
     select_demultiplex_table    table_;
-    MockReactor                 reactor_;
-    MockEventHandler	        handler1_;
-    MockEventHandler	        handler2_;
-
+    Reactor	                    *reactor_;
+    MockEventHandler            *handler1_;
+    MockEventHandler	        *handler2_;
 };
 
 TEST_F(select_demultiplex_table_test, test_bind){
     int handle1 = 1, handle2 = 2;
-    table_.bind(handle1, &this->handler1_, event_handler::READ_EVENT);
+    unsigned int event_type1 = event_handler::READ_EVENT;
+    table_.bind(handle1, this->handler1_, event_type1);
     ASSERT_EQ(table_.get_current_max_handle_p_1(), 2);
 
-    auto event_tuple = table_.get_event_vector();
+    auto event_vector = table_.get_event_vector();
+    ASSERT_EQ(8, event_vector.size());//size
+    ASSERT_EQ(table_.get_handler(handle1, event_type1), this->handler1_);//handler ptr
+    //select_event_tuple shoule be right
+    ASSERT_EQ(event_vector[handle1].types_and_handlers.size(), 64);
+    ASSERT_EQ(event_vector[handle1].types_and_handlers[event_type1], this->handler1_);
+    ASSERT_EQ(event_vector[handle1].event_count, 1);
+    //other handles and other types should be right
+    for(int handle = 0; handle < event_vector.size(); handle++){
+        if(handle == 1) continue;
+        ASSERT_EQ(event_vector[handle].types_and_handlers.size(), 64);
+        ASSERT_EQ(event_vector[handle].event_count, 0);
+        ASSERT_EQ(event_vector[handle].types_and_handlers[event_type1], nullptr);
+    }
+
+    //bind the second handle
+    unsigned int event_type2 = event_handler::WRITE_EVENT;
+    table_.bind(handle2, this->handler2_, event_type2);
+
+    //table_ property
+    ASSERT_EQ(table_.get_current_max_handle_p_1(), 3);
+    auto event_vector2 = table_.get_event_vector();
+    ASSERT_EQ(8, event_vector2.size());
+    //--------------for handle1 parts
+    ASSERT_EQ(table_.get_handler(handle1, event_type1), this->handler1_);//handler ptr
+    //select_event_tuple shoule be right
+    ASSERT_EQ(event_vector2[handle1].types_and_handlers.size(), 64);
+    ASSERT_EQ(event_vector2[handle1].types_and_handlers[event_type1], this->handler1_);
+    ASSERT_EQ(event_vector2[handle1].event_count, 1);
+    //--------------for handle1 parts
+    for(int handle = 0; handle < event_vector2.size(); handle++){
+        if(handle == 1 || handle == 2) continue;
+        ASSERT_EQ(event_vector[handle].types_and_handlers.size(), 64);
+        ASSERT_EQ(event_vector[handle].event_count, 0);
+        ASSERT_EQ(event_vector[handle].types_and_handlers[event_type1], nullptr);
+    }
+
+
 }
