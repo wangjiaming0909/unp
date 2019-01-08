b9495e28b797a0a2ae22c7be796892afe0eccb84
wangjiaming0909 <604227650@qq.com>
2019-01-08 17:12:19 +0800

    enhance read_handler

--- server/reactor/io_handler.h
+++ server/reactor/io_handler.h
@@ -32,6 +32,12 @@ public:
     // virtual int handle_signal(int handle);
     virtual int get_handle() const { return this->peer_.get_handle(); }
     // virtual void set_handle(int handle){ }
+    bool is_handle_valid(int handle){
+        if(handle != peer_.get_handle()) {
+            LOG(ERROR) << "INVALID handle specified: " << handle << " existed: " << peer_.get_handle();
+            return -1;
+        }
+    }
 
 protected:
     net::sock_stream peer_;
