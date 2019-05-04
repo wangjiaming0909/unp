86e533d930c7fcab6d3e492149c26b59c44e25f3
wangjiaming0909 <604227650@qq.com>
2019-05-04 10:28:56 +0800

    tcp_server

--- server/reactor/tcp_server.cpp
+++ server/reactor/tcp_server.cpp
@@ -1,19 +1,32 @@
 #include "tcp_server.h"
+#include "select_reactor_impl.h"
+#include "poll_reactor_impl.h"
+#include "util/easylogging++.h"
 
 namespace reactor
 {
 
 tcp_server::tcp_server(const net::inet_addr& local_addr)
-    : reactor_()
+    : reactor_(0)
     , acceptor_()
     , pool_()
     , local_addr_(local_addr)
 {
 }
 
+tcp_server::~tcp_server()
+{
+    acceptor_.reset();
+    delete reactor_;
+}
+
 int tcp_server::open()
 {
-    
+    if(make_reactor() != 0)
+        return -1;
+
+    make_acceptor();
+    pool_ = std::make_shared<thread::thread_pool>(thread_num_);
 }
 
 int tcp_server::close()
@@ -21,7 +34,41 @@ int tcp_server::close()
 
 }
 
+int tcp_server::make_reactor()
+{
+    switch(reactor_t_)
+    {
+        case reactor_imp_t_enum::USING_SELECT:
+            reactor_ = new Reactor(new select_reactor_impl{});
+            break;
+        case reactor_imp_t_enum::USING_POLL:
+            reactor_ = new Reactor(new poll_reactor_impl{});
+            break;
+        case reactor_imp_t_enum::USING_EPOLL:
+            reactor_ = new Reactor(new epoll_reactor_impl{});
+            break;
+        default:
+            reactor_ = 0;
+            return -1;
+    }
+    return 0;
+}
 
+void tcp_server::make_acceptor()
+{
+    acceptor_ = std::make_shared<acceptor>(*reactor_, local_addr_);
+}
+
+void tcp_server::set_thread_num(size_t n)
+{
+    if(n == 0 || n > 32)
+    {
+        LOG(WARNING) << "thread num can't be large than 32 or can't be 0: " << n;
+        n = DEFAULT_THREAD_NUM;
+    }
+
+    thread_num_ = n;
+}
 
 
 
--- server/reactor/tcp_server.h
+++ server/reactor/tcp_server.h
@@ -16,31 +16,42 @@ enum class reactor_imp_t_enum
     USING_SELECT, USING_POLL, USING_EPOLL
 };
 
+/*
+    reactor的个数应该与 thread 的个数相同，因为每个thread里会放一个reactor
+    其中，处理listen事件的线程为第一线程，此线程会在 reactor上放 acceptor
+*/
+
 class tcp_server
 {
 public:
-    using reactor_ptr_t	= std::shared_ptr<Reactor>;
+    using reactor_ptr_t	= Reactor*;
     using acceptor_ptr_t = std::shared_ptr<acceptor>;
+    using pool_ptr_t = std::shared_ptr<thread::thread_pool>;
+    static const int DEFAULT_THREAD_NUM = 4;
 public:
     tcp_server(const net::inet_addr& local_addr);
+    ~tcp_server();
 
 public:
     //must call before open
     void set_reactor_type(reactor_imp_t_enum reactor_t) {reactor_t_ = reactor_t;}
-    void set_thread_num(size_t n) {thread_num_ = n;}
+    void set_thread_num(size_t n);
 
-public:
     int open();
     int close();
 
 private:
+    int make_reactor();
+    void make_acceptor();
+
+private:
     reactor_imp_t_enum 			reactor_t_ = reactor_imp_t_enum::USING_EPOLL;
-    size_t						thread_num_ = 8;
+    size_t						thread_num_ = DEFAULT_THREAD_NUM;
 
 private:
     reactor_ptr_t				reactor_;
     acceptor_ptr_t 				acceptor_;
-    thread::thread_pool			pool_;
+    pool_ptr_t          		pool_;
     net::inet_addr 				local_addr_;
 };
 
