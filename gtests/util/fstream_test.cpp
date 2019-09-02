1d082a84eb9ac227d9969277df54eb3545af03fc
wangjiaming0909 <604227650@qq.com>
2019-09-02 09:57:37 +0800

    fstream test

--- gtests/util/fstream_test.cpp
+++ gtests/util/fstream_test.cpp
@@ -1,7 +1,57 @@
 #include <gtest/gtest.h>
 #include <fstream>
+#include "util/timer.h"
 
 using namespace std;
+
+class FileWriter
+{
+public:
+    using Size_t =  std::streamsize;
+    FileWriter(const char* fileName) 
+        : fileName_(fileName)
+        , fstream_()
+    {
+        if(fileName != nullptr) valid_ = true;
+    }
+
+    void setBufAndOpen(void* buf, Size_t size)
+    {
+        if(!valid_) return;
+        fstream_.rdbuf()->pubsetbuf(static_cast<char*>(buf), size);
+        fstream_.open(fileName_, fstream_.out | fstream_.in | fstream_.trunc);
+    }
+
+    void write(const char* data)
+    {
+        fstream_ << data;
+    }
+
+    FileWriter& write(const char* data, Size_t size)
+    {
+        if(!valid_) return *this;
+        auto streamBuf = fstream_.rdbuf();
+        streamBuf->sputn(data, size);
+
+        return *this;
+    }
+
+    void flush()
+    {
+        if(!valid_) return;
+        fstream_.flush();
+    }
+    void close()
+    {
+        fstream_.flush();
+        fstream_.close();
+    }
+
+private:
+    std::string fileName_;
+    std::fstream fstream_;
+    bool valid_ = false;
+};
 
 TEST(Fstream, normal)
 {
@@ -21,5 +71,37 @@ TEST(Fstream, normal)
     else 
     {
         FAIL();
+    }
+}
+
+TEST(FStream, normal2)
+{
+    {
+        utils::timer _{"writing with manual flush"};
+        const char* fileName = "/tmp/2.txt";
+        void* buf = ::calloc(10*1024*1024, 1);
+        FileWriter writer{fileName}; 
+        writer.setBufAndOpen(buf, 10*1024*1024);
+        const char* data = "1234567890\n";
+        auto size = strlen(data);
+        for(int i = 0; i < (1024*1024*20); i++)
+        {
+            writer.write(data, size);
+        }
+        writer.close();
+        free(buf);
+    }
+    {
+        utils::timer _{"writing with auto flush"};
+        const char* fileName = "/tmp/3.txt";
+        void *buf = ::calloc(10*1024*1024, 1);
+        FileWriter writer{fileName};
+        writer.setBufAndOpen(buf, 10*1024*1024);
+        for(int i = 0; i < (1024*1024*20); i++)
+        {
+            writer.write("1234567890\n");
+        }
+        writer.close();
+        free(buf);
     }
 }
