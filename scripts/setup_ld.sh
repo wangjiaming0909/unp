dcef16d06434a04a05c7d9602d61261428c86257
wangjiaming0909 <604227650@qq.com>
2019-11-04 18:39:42 +0800

    fix pipe line

--- scripts/setup_ld.sh
+++ scripts/setup_ld.sh
@@ -2,5 +2,8 @@
 
 touch /etc/ld.so.conf.d/lib64.conf && 
 echo '/usr/local/lib64' > /etc/ld.so.conf.d/lib64.conf &&
+
+touch /etc/ld.so.conf.d/boost.conf &&
+echo '/boost_1_70_0/stage/lib' > /etc/ld.so.conf.d/boost.conf &&
 ldconfig
 
