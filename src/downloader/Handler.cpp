8633453248d58420ff8f2aebeb8a675ca25a5f2b
wangjiaming0909 <604227650@qq.com>
2019-12-09 23:34:34 +0800

    refactoring downloader::Handler

---
+++ src/downloader/Handler.cpp
@@ -0,0 +1,219 @@
+#include "Handler.h"
+#include "http/gperf/HttpHeaderCode.h"
+#include "util/easylogging++.h"
+#include <utility>
+
+namespace downloader
+{
+
+Handler::Handler(reactor::Reactor& react, const std::string& url) 
+    : connection_handler(react)
+    , request_{}
+    , codec_{http::HttpDirection::DOWNSTREAM}
+    , urlParser_{}
+    , url_{url}
+{
+    codec_.setCallback(this);
+    urlParser_.init(url_);
+    if (!urlParser_.valid())
+    {
+        url_ = "";
+        LOG(WARNING) << "url format error...";
+        return;
+    }
+}
+
+int Handler::handle_input(int handle)
+{
+    auto ret = connection_handler::handle_input(handle);
+    if(ret < 0)
+    {
+        LOG(ERROR) << "error in handle_input...";
+        return -1;
+    }
+    if(ret == 0 || input_buffer_.buffer_length() == 0)
+    {
+        LOG(WARNING) << "no data, retrying...";
+        if(isShouldClose_)
+            return -1;
+        return 0;
+    }
+
+    while(input_buffer_.buffer_length() > 0)
+    {
+        auto firstChain = input_buffer_.begin().chain();
+        auto data = firstChain.get_start_buffer();
+        auto chainLen = firstChain.size();
+        if(data != nullptr && chainLen != 0)
+        {
+            std::string s{static_cast<char*>(data), chainLen};
+            LOG(INFO) << "----------------received: " << s;
+        }
+        string_piece::const_string_piece sp{static_cast<const char*>(data), chainLen};
+        size_t bytesRead = codec_.onIngress(sp);
+        if(codec_.hasError())
+        {
+            LOG(WARNING) << "Http parser parse error";
+            codec_.pause(1);
+            return -1;
+        }
+        assert(bytesRead == chainLen);
+        input_buffer_.drain(chainLen);
+    }
+    if(isShouldClose_) 
+        return -1;
+    return 0;
+}
+
+int Handler::open()
+{
+    request_.setHttpVersion(1, 1);
+    request_.setRequestPath(urlParser_.path().cbegin());
+    request_.setRequestMethod(http::HTTPMethod::GET);
+    request_.addHeader(http::HttpHeaderCode::HTTP_HEADER_HOST, urlParser_.host());
+    request_.addHeader(http::HttpHeaderCode::HTTP_HEADER_USER_AGENT, USERAGENT);
+    request_.addHeader(http::HttpHeaderCode::HTTP_HEADER_ACCEPT, ACCEPT);
+    request_.addHeader(http::HttpHeaderCode::HTTP_HEADER_ACCEPT_ENCODING, ACCEPTENCODING);
+    if(usingRangeDownload_)
+    {
+        request_.addHeader(
+            http::HttpHeaderCode::HTTP_HEADER_RANGE, 
+            "range: bytes=" 
+            + std::to_string(rangeBegin_) 
+            + "-" 
+            + std::to_string(rangeEnd_));
+    }
+    auto messageStr = request_.buildRequestMessage();
+    LOG(INFO) << *messageStr;
+    auto bytesWritten = write(messageStr->c_str(), messageStr->size());
+    if (bytesWritten <= 0) return -1;
+    status_ = HandlerStatus::REQUEST_SENT;
+
+    return enable_reading();
+}
+
+int Handler::onStatus(const char* buf, size_t len)
+{
+    switch(codec_.status())
+	{
+		case 302:
+			status_ = HandlerStatus::RECEIVED302;
+			return 0;
+		case 200:
+			status_ = HandlerStatus::RECEIVED200;
+			return 0;
+		default:
+			codec_.pause(1);
+			return -1;
+	}
+}
+
+int Handler::onBody(const char* buf, size_t size)
+{
+	//if(status_ == )
+}
+
+int Handler::onHeadersComplete(size_t len)
+{
+	if(status_ == HandlerStatus::RECEIVED302)
+	{
+		auto* locationHeader = codec_.message().getHeaderValue(http::HttpHeaderCode::HTTP_HEADER_LOCATION);
+		if(locationHeader == nullptr) 
+		{
+			LOG(INFO) << "server returned 302, but no location header...";
+			isShouldClose_ = true;
+			status_ = HandlerStatus::RECV302_WITHOUT_NEW_LOCATION;
+			return -1;
+		}
+		url_ = *locationHeader;
+		status_ = HandlerStatus::NEW_LOCATION_GOT;
+		isShouldClose_ = true;
+	}else if(status_ == HandlerStatus::RECEIVED200)
+	{
+		auto* cd = codec_.message().getHeaderValue(http::HttpHeaderCode::HTTP_HEADER_CONTENT_DISPOSITION);
+		if(cd == nullptr) 
+		{
+			LOG(WARNING) << "no HTTP_HEADER_CONTENT_DISPOSITION heade...";
+			status_ = HandlerStatus::NO_CONTENT_DISPOSITION;
+			return -1;
+		}
+		retriveFileNameFromContentDisposition(*cd);
+
+		if(usingRangeDownload_)
+		{
+			if(*codec_.message().getHeaderValue(http::HttpHeaderCode::HTTP_HEADER_TRANSFER_ENCODING) == "chunked")
+			{
+				status_ = HandlerStatus::CHUNK_ENCODING;
+				return 0;
+			}
+			auto* contentRangeHeader = codec_.message().getHeaderValue(http::HttpHeaderCode::HTTP_HEADER_CONTENT_RANGE);
+			if(contentRangeHeader == nullptr)
+			{
+				LOG(ERROR) << "trying to get data with range header, but server didn't respond with range...";
+				status_ = HandlerStatus::NOT_RESPONDING_TO_RANGE;
+				return 0;
+			}
+			auto pair = parseContentRangeHeader(*contentRangeHeader);
+			if(pair.first != rangeBegin_ && pair.second != rangeEnd_)
+			{
+				LOG(ERROR) << "Range error preset begin: " << rangeBegin_ << " end: " << rangeEnd_ << " got begin: " << pair.first << " end: " << pair.second;
+				status_ = HandlerStatus::RANGE_NOT_MATCH;
+				return -1;
+			}
+			status_ = HandlerStatus::RANGE_MATCH;
+		}
+	}
+	return 0;
+}
+
+std::pair<uint64_t, uint64_t> Handler::parseContentRangeHeader(const std::string& headerValue)
+{
+	return std::make_pair(rangeBegin_, rangeEnd_);
+
+}
+
+void Handler::retriveFileNameFromContentDisposition(const std::string& cd)
+{
+    auto it = std::find(cd.begin(), cd.end(), '=');
+    if(it == cd.end())
+    {
+        LOG(WARNING) << "Unknown content disposition...";
+        fileName_ = DEFAULT_FILE_NAME;
+    }
+    std::string::const_iterator begin = ++it;
+    std::string::const_iterator end = cd.end();
+    if (*(it) == '"' || *(it) == '\'')
+    {
+        begin = ++it;
+    }
+    if(cd.back() == '"' || cd.back() == '\'')
+    {
+        end--;
+    }
+    fileName_ = std::string{begin, end};
+}
+
+int Handler::onHeaderField(const char *buf, size_t len)
+{
+}
+
+int Handler::onHeaderValue(const char *buf, size_t len)
+{
+
+}
+
+int Handler::onMessageComplete()
+{
+
+}
+
+int Handler::onChunkHeader(size_t len)
+{
+
+}
+
+int Handler::onChunkComplete()
+{
+
+}
+} 
---
+++ src/downloader/Handler.h
@@ -0,0 +1,80 @@
+#pragma once
+#include "reactor/connection_handler.h"
+#include "http/Http1xCodec.h"
+#include "http/HttpMessage.h"
+#include "http/http_parser/URLParser.h"
+
+namespace downloader
+{
+class Handler : public reactor::connection_handler, public http::HttpCodec::Callback
+{
+
+enum class HandlerStatus
+{
+    IDLE = 0,
+    REQUEST_SENT,
+    RECEIVED302,
+    NEW_LOCATION_GOT,
+	RECV302_WITHOUT_NEW_LOCATION,
+    RECEIVED200,
+	CHUNK_ENCODING,
+	NOT_RESPONDING_TO_RANGE,
+	RANGE_NOT_MATCH,
+	RANGE_MATCH,
+	PARTIALLY_DATA_GOT,
+	NO_CONTENT_DISPOSITION
+};
+
+public:
+    Handler(reactor::Reactor& react, const std::string& url);
+    virtual ~Handler() = default;
+
+    virtual int handle_input(int handle) override;
+    virtual int open() override;
+
+    virtual int onStatus(const char* buf, size_t len) override;
+    virtual int onBody(const char* buf, size_t size) override;
+    virtual int onHeadersComplete(size_t len) override;
+    virtual int onMessageComplete() override;
+    virtual int onHeaderField(const char *buf, size_t len) override;
+    virtual int onHeaderValue(const char *buf, size_t len) override;
+    int onChunkHeader(size_t len) override;
+    int onChunkComplete() override;
+
+    void setDownloadRange(uint64_t begin, uint64_t end)
+    {
+       usingRangeDownload_ = true;
+       rangeBegin_ = begin;
+       rangeEnd_ = end;
+    }
+
+
+private:
+	std::pair<uint64_t, uint64_t> parseContentRangeHeader(const std::string& headerValue);
+	void retriveFileNameFromContentDisposition(const std::string& cd);
+
+private:
+    uint64_t fileSize_ = 0;
+    uint64_t rangeBegin_ = 0;
+    uint64_t rangeEnd_ = 0;
+    uint64_t bytesDownloaded_ = 0;
+
+    bool isShouldClose_ = false;
+    bool usingRangeDownload_ = false;
+
+    http::HttpMessage request_;
+    http::Http1xCodec codec_;
+    http::URLParser urlParser_;
+    std::string url_;
+    HandlerStatus status_ = HandlerStatus::IDLE;
+	std::string fileName_;
+};
+static string_piece::const_string_piece USERAGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0";
+static string_piece::const_string_piece ACCEPT = "*/*";
+// static string_piece::const_string_piece ACCEPTENCODING = "gzip, deflate, br";
+static string_piece::const_string_piece ACCEPTENCODING = "identity";
+static string_piece::const_string_piece ACCEPTLANGUAGE = "en-US,en;q=0.5";
+static const std::string DEFAULT_FILE_NAME = "unp.unp";
+// static string_piece::const_string_piece CONNECTION = "keep-alive";
+
+}
