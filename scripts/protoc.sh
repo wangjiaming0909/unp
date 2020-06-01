36665913044a42c516933b69c845cc0e6a8b22a9
wangjiaming <jiaming.wang@greatopensource.com>
2020-06-01 09:21:36 +0800

    pipeline

--- scripts/protoc.sh
+++ scripts/protoc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ~/codes/unp/scripts
+cd ./scripts
 protoc -I=../src/proto/ --cpp_out=../src/proto/ ../src/proto/mess_wl.proto && mv ../src/proto/mess_wl.pb.cc ../src/proto/mess_wl.pb.cpp
 protoc -I=../src/proto/ --cpp_out=../src/proto/ ../src/proto/mess.proto && mv ../src/proto/mess.pb.cc ../src/proto/mess.pb.cpp
 protoc -I=../src/proto/ --cpp_out=../src/proto/ ../src/proto/sync_mess.proto \
